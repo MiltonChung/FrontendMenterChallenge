04cce0dca336bc155f7bfc97b66fcc6516ed51a7
Milton Chung <miltonjchung@gmail.com>
2020-10-10 23:01:23 -0700

    added some animations

--- FaqAccordionCard/main.js
+++ FaqAccordionCard/main.js
@@ -8,26 +8,31 @@ function expand(n) {
 	switch (n) {
 		case 1:
 			questionOne.classList.toggle("show");
+			document.getElementById("questionOneButton").classList.toggle("rotate");
 			document.querySelector(".questionOneQ p").classList.toggle("bold");
 			break;
 
 		case 2:
 			questionTwo.classList.toggle("show");
+			document.getElementById("questionTwoButton").classList.toggle("rotate");
 			document.querySelector(".questionTwoQ p").classList.toggle("bold");
 			break;
 
 		case 3:
 			questionThree.classList.toggle("show");
+			document.getElementById("questionThreeButton").classList.toggle("rotate");
 			document.querySelector(".questionThreeQ p").classList.toggle("bold");
 			break;
 
 		case 4:
 			questionFour.classList.toggle("show");
+			document.getElementById("questionFourButton").classList.toggle("rotate");
 			document.querySelector(".questionFourQ p").classList.toggle("bold");
 			break;
 
 		case 5:
 			questionFive.classList.toggle("show");
+			document.getElementById("questionFiveButton").classList.toggle("rotate");
 			document.querySelector(".questionFiveQ p").classList.toggle("bold");
 			break;
 
