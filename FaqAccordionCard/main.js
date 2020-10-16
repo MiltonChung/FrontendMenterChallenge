75ccd15485e25667e39ed43477852d9ee0e8bc73
Milton Chung <miltonjchung@gmail.com>
2020-10-15 22:35:08 -0700

    updated faq accordion card

--- FaqAccordionCard/main.js
+++ FaqAccordionCard/main.js
@@ -1,42 +1,69 @@
-let questionOne = document.getElementById("questionOne");
-let questionTwo = document.getElementById("questionTwo");
-let questionThree = document.getElementById("questionThree");
-let questionFour = document.getElementById("questionFour");
-let questionFive = document.getElementById("questionFive");
+const questions = document.querySelectorAll(".card-question");
 
-function expand(n) {
-	switch (n) {
-		case 1:
-			questionOne.classList.toggle("show");
-			document.getElementById("questionOneButton").classList.toggle("rotate");
-			document.querySelector(".questionOneQ p").classList.toggle("bold");
-			break;
+questions.forEach((question) => {
+	const questionBtn = question.querySelector(".question-btn");
+	const title = question.querySelector(".title");
+	questionBtn.classList.toggle("rotate");
 
-		case 2:
-			questionTwo.classList.toggle("show");
-			document.getElementById("questionTwoButton").classList.toggle("rotate");
-			document.querySelector(".questionTwoQ p").classList.toggle("bold");
-			break;
+	//Event listener for the questions.
+	title.addEventListener("click", () => {
+		showAnswer();
+	});
 
-		case 3:
-			questionThree.classList.toggle("show");
-			document.getElementById("questionThreeButton").classList.toggle("rotate");
-			document.querySelector(".questionThreeQ p").classList.toggle("bold");
-			break;
+	//Event listener for the buttons.
+	questionBtn.addEventListener("click", () => {
+		showAnswer();
+	});
 
-		case 4:
-			questionFour.classList.toggle("show");
-			document.getElementById("questionFourButton").classList.toggle("rotate");
-			document.querySelector(".questionFourQ p").classList.toggle("bold");
-			break;
-
-		case 5:
-			questionFive.classList.toggle("show");
-			document.getElementById("questionFiveButton").classList.toggle("rotate");
-			document.querySelector(".questionFiveQ p").classList.toggle("bold");
-			break;
-
-		default:
-			break;
+	function showAnswer() {
+		questions.forEach((item) => {
+			if (item != question) {
+				item.classList.remove("show");
+			}
+		});
+		question.classList.toggle("show");
 	}
-}
+});
+
+// let questionOne = document.getElementById("questionOne");
+// let questionTwo = document.getElementById("questionTwo");
+// let questionThree = document.getElementById("questionThree");
+// let questionFour = document.getElementById("questionFour");
+// let questionFive = document.getElementById("questionFive");
+
+// function expand(n) {
+// 	switch (n) {
+// 		case 1:
+// 			questionOne.classList.toggle("show");
+// 			document.getElementById("questionOneButton").classList.toggle("rotate");
+// 			document.querySelector(".questionOneQ p").classList.toggle("bold");
+// 			break;
+
+// 		case 2:
+// 			questionTwo.classList.toggle("show");
+// 			document.getElementById("questionTwoButton").classList.toggle("rotate");
+// 			document.querySelector(".questionTwoQ p").classList.toggle("bold");
+// 			break;
+
+// 		case 3:
+// 			questionThree.classList.toggle("show");
+// 			document.getElementById("questionThreeButton").classList.toggle("rotate");
+// 			document.querySelector(".questionThreeQ p").classList.toggle("bold");
+// 			break;
+
+// 		case 4:
+// 			questionFour.classList.toggle("show");
+// 			document.getElementById("questionFourButton").classList.toggle("rotate");
+// 			document.querySelector(".questionFourQ p").classList.toggle("bold");
+// 			break;
+
+// 		case 5:
+// 			questionFive.classList.toggle("show");
+// 			document.getElementById("questionFiveButton").classList.toggle("rotate");
+// 			document.querySelector(".questionFiveQ p").classList.toggle("bold");
+// 			break;
+
+// 		default:
+// 			break;
+// 	}
+// }
