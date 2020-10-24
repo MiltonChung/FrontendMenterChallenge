e73afef6aae89aa9bc64f7efed38f09572229754
Milton Chung <miltonjchung@gmail.com>
2020-10-23 19:12:27 -0700

    finished sign up form component

--- SignUpFormComponent/main.js
+++ SignUpFormComponent/main.js
@@ -3,22 +3,77 @@ const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+const rowFirstName = document.getElementById("rowFn");
+const rowLastName = document.getElementById("rowLn");
+const rowEmail = document.getElementById("rowEmail");
+const rowPassword = document.getElementById("rowPw");
+
+const form = document.getElementById("form");
+
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", validateForm);
 
+let firstNameTracker = false,
+	lastNameTracker = false,
+	emailTracker = false,
+	passwordTracker = false;
+
 function validateForm(e) {
 	e.preventDefault();
-	const userInput = document.getElementById("user-input").value;
-	console.log(userInput);
-	if (!ValidateEmail(userInput)) {
-		emailForm.classList.add("error");
+	const firstNameInput = firstName.value;
+	if (firstNameInput == "" || firstNameInput.length === 0) {
+		form.classList.add("error");
+		rowFirstName.classList.add("showError");
+		firstNameTracker = false;
 	} else {
-		emailForm.classList.remove("error");
-		emailForm.classList.add("success");
-		document.getElementById("user-input").value = "";
-		setTimeout(function () {
-			emailForm.classList.remove("success");
-		}, 2000);
+		rowFirstName.classList.remove("showError");
+		firstNameTracker = true;
+	}
+
+	const lastNameInput = lastName.value;
+	if (lastNameInput == "" || lastNameInput.length === 0) {
+		form.classList.add("error");
+		rowLastName.classList.add("showError");
+		lastNameTracker = false;
+	} else {
+		rowLastName.classList.remove("showError");
+		lastNameTracker = true;
+	}
+
+	const emailInput = email.value;
+	if (!ValidateEmail(emailInput)) {
+		form.classList.add("error");
+		rowEmail.classList.add("showError");
+		emailTracker = false;
+	} else {
+		rowEmail.classList.remove("showError");
+		emailTracker = true;
+	}
+
+	const passwordInput = password.value;
+	if (passwordInput == "" || passwordInput.length === 0) {
+		form.classList.add("error");
+		rowPassword.classList.add("showError");
+		passwordTracker = false;
+	} else {
+		rowPassword.classList.remove("showError");
+		passwordTracker = true;
+	}
+
+	checkIfSuccess();
+}
+
+function checkIfSuccess() {
+	if (firstNameTracker && lastNameTracker && emailTracker && passwordTracker) {
+		form.classList.remove("error");
+		form.classList.add("success");
+		firstName.value = "";
+		lastName.value = "";
+		email.value = "";
+		password.value = "";
+		setTimeout(() => {
+			form.classList.remove("success");
+		}, 2500);
 	}
 }
 
