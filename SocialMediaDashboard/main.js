51d2788084b1c7492d32c8b196a740a61d7aa084
Milton Chung <miltonjchung@gmail.com>
2021-02-17 22:07:43 -0800

    dashboard responsive

--- SocialMediaDashboard/main.js
+++ SocialMediaDashboard/main.js
@@ -4,7 +4,6 @@ const toggle = document.querySelector(".toggle-control");
 let toggleState = true;
 
 toggle.addEventListener("click", e => {
-	console.log(toggleState);
 	if (toggleState) {
 		body.classList.toggle("dark-mode");
 	}
