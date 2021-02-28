e65aac8989c2325e82df95869e2f9624c122972b
Milton Chung <miltonjchung@gmail.com>
2021-02-27 19:49:10 -0800

    search function working

--- IPAddressTracker/script.js
+++ IPAddressTracker/script.js
@@ -1,25 +1,107 @@
+const displayIpAddress = document.querySelector("#ipAddress");
+const displayLocation = document.querySelector("#location");
+const displayTimezone = document.querySelector("#timezone");
+const displayISP = document.querySelector("#isp");
+const submitSearch = document.getElementById("search_form");
+
 // IP Geolocation API by IPify ========================================
 // Docs: https://geo.ipify.org/docs
 // Example: https://geo.ipify.org/api/v1?apiKey=at_4qMCbIQ94gKTv5J6AcTsZX35Brtba&ipAddress=8.8.8.8
-
 const BASE_URL = "https://geo.ipify.org/api/v1?apiKey=";
 const IP_GEOLOCATION_API = "at_4qMCbIQ94gKTv5J6AcTsZX35Brtba";
-const IP_ADDRESS = "";
-const DOMAIN = "";
+let LAT, LONG, MAP, MARKER;
 
-// Leaflet + Mapbox api calls =========================================
-// Docs: https://leafletjs.com/examples/quick-start/
-const mymap = L.map("map").setView([51.505, -0.09], 13);
+window.onload = () => {
+	fetch(`${BASE_URL}${IP_GEOLOCATION_API}`)
+		.then(res => res.json())
+		.then(data => {
+			setHtmlInfo(data.ip, data.location, data.location.timezone, data.isp);
+			setMap(data.location.lat, data.location.lng);
+		});
+};
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-	attribution:
-		'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	id: "mapbox/streets-v11",
-	tileSize: 512,
-	zoomOffset: -1,
-	accessToken:
-		"pk.eyJ1IjoiY3Jvc3NmZmlyZXgiLCJhIjoiY2tsbHBpd2JkMDFreTJvcDRlbjNseGc1dSJ9.a26lI9C3F4W49wSaehHoMA",
-}).addTo(mymap);
+submitSearch.addEventListener("submit", e => {
+	e.preventDefault();
+	const userInput = e.target[0].value;
 
-const marker = L.marker([51.5, -0.09]).addTo(mymap);
+	if (ValidateIPaddress(userInput)) {
+		fetch(`${BASE_URL}${IP_GEOLOCATION_API}&ipAddress=${userInput}`)
+			.then(res => res.json())
+			.then(data => {
+				setHtmlInfo(data.ip, data.location, data.location.timezone, data.isp);
+				FlyToMap(data.location.lat, data.location.lng);
+			})
+			.catch(err => console.log(err));
+	} else if (userInput) {
+		fetch(`${BASE_URL}${IP_GEOLOCATION_API}&domain=${userInput}`)
+			.then(res => res.json())
+			.then(data => {
+				setHtmlInfo(data.ip, data.location, data.location.timezone, data.isp);
+				FlyToMap(data.location.lat, data.location.lng);
+			})
+			.catch(err => console.log(err));
+	} else if (userInput === "") {
+		alert("Please enter something");
+	} else {
+		alert("Please enter a valid IP address or domain");
+	}
+});
+
+function setHtmlInfo(ip, location, timezone, isp) {
+	displayIpAddress.innerHTML = ip;
+	displayLocation.innerHTML = `${location.city}, ${location.region} ${location.postalCode}`;
+	displayTimezone.innerHTML = `UTC ${timezone}`;
+	displayISP.innerHTML = isp;
+}
+
+function setMap(lat, long) {
+	// Leaflet + Mapbox api calls =========================================
+	// Docs: https://leafletjs.com/examples/quick-start/
+	MAP = L.map("map").setView([lat, long], 13);
+
+	L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+		attribution:
+			'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: "mapbox/streets-v11",
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken:
+			"pk.eyJ1IjoiY3Jvc3NmZmlyZXgiLCJhIjoiY2tsbHBpd2JkMDFreTJvcDRlbjNseGc1dSJ9.a26lI9C3F4W49wSaehHoMA",
+	}).addTo(MAP);
+
+	MARKER = L.marker([lat, long]).addTo(MAP);
+}
+
+function FlyToMap(lat, long) {
+	MAP.flyTo([lat, long], 13, {
+		animate: true,
+		duration: 0.8, // in seconds
+	});
+
+	MARKER.setLatLng([lat, long]);
+}
+
+function ValidateIPaddress(ipaddress) {
+	if (
+		/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+			ipaddress
+		)
+	) {
+		return true;
+	}
+	return false;
+}
+
+function isValidHttpUrl(string) {
+	let url;
+
+	try {
+		url = new URL(string);
+		console.log(url);
+	} catch (_) {
+		return false;
+	}
+
+	return url.host;
+}
