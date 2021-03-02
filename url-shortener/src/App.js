74c51ba6e0f06d3654a0745b2044f4af135dd63a
Milton Chung <miltonjchung@gmail.com>
2021-03-02 00:02:26 -0800

    test

--- url-shortener/src/App.js
+++ url-shortener/src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import "./styles/main.scss";
-
+// https://app.shrtco.de/docs
 function App() {
 	return (
 		<div className="app">
