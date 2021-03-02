c571abb9f99e6517beb32bd03b696e7c3a6d2eee
Milton Chung <miltonjchung@gmail.com>
2021-03-02 00:42:43 -0800

    test

--- url-shortener/src/components/Nav.js
+++ url-shortener/src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
