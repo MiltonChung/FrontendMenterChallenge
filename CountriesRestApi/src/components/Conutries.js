ca2d3dcbc6ea71f90a3fee3303b23d4e3a9ab530
Milton Chung <56741854+MiltonChung@users.noreply.github.com>
2021-04-11 19:01:24 -0700

    filter done

--- CountriesRestApi/src/components/Conutries.js
+++ CountriesRestApi/src/components/Conutries.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter.js";
+import { ALL_URL } from "../base_url.js";
 
 const Conutries = () => {
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		fetch(ALL_URL)
+			.then(res => res.json())
+			.then(data => {
+				setCountries(data.slice(0, 15));
+				console.log(countries);
+			});
+	}, []);
+
 	return (
 		<main>
 			<div className="main-container">
